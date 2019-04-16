0eec65bb2718ab850bc11bbc159629fd26e10286
Andrew Bay <daddnbay@yahoo.com>
2019-04-16 23:57:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+ []
 end
 
 def create_an_array
-  
+ ["Dad", "Mom", "Daughter", "Son"] 
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["Dad", "Mom"]
+  element = "Daughter"
 end
 
 def add_element_to_start_of_array(array, element)
