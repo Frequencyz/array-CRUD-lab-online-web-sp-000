0833876ab7e65d2809f3c86980dc9e76f27a4aa4
Andrew Bay <daddnbay@yahoo.com>
2019-04-17 00:12:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["name", "is", "mud"]
-  array.unshift ("My")
+  ["my", "name", "is", "mud"]
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
